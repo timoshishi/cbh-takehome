b7c69e53470b397f8b5a8de9099548508cfdbeba
timoshishi <timfrrst@gmail.com>
2022-11-25 09:35:29 -0500

    tests: add tests

--- dpk.js
+++ dpk.js
@@ -1,7 +1,13 @@
-const crypto = require("crypto");
+const crypto = require('crypto');
 
+/**
+ * @describe Returns a deterministic partition key for a given input string or object
+ * @param {Object|string} [event] - The string to be hashed
+ * @param {string} [event.partitionKey] - The string to be hashed
+ * @returns {string} - The partition key
+ */
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
+  const TRIVIAL_PARTITION_KEY = '0';
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
@@ -10,19 +16,19 @@ exports.deterministicPartitionKey = (event) => {
       candidate = event.partitionKey;
     } else {
       const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+      candidate = crypto.createHash('sha3-512').update(data).digest('hex');
     }
   }
 
   if (candidate) {
-    if (typeof candidate !== "string") {
+    if (typeof candidate !== 'string') {
       candidate = JSON.stringify(candidate);
     }
   } else {
     candidate = TRIVIAL_PARTITION_KEY;
   }
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    candidate = crypto.createHash('sha3-512').update(candidate).digest('hex');
   }
   return candidate;
 };
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,56 @@
-const { deterministicPartitionKey } = require("./dpk");
-
-describe("deterministicPartitionKey", () => {
+const { deterministicPartitionKey } = require('./dpk');
+const crypto = require('crypto');
+describe('deterministicPartitionKey', () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
+    expect(trivialKey).toBe('0');
+  });
+
+  it('Always returns a string', () => {
+    const trivialKey = deterministicPartitionKey('123');
+    expect(typeof trivialKey).toBe('string');
+  });
+
+  it('Returns the same value if the same input is passed in twice', () => {
+    const trivialKey = deterministicPartitionKey('123');
+    const trivialKey2 = deterministicPartitionKey('123');
+    expect(trivialKey).toBe(trivialKey2);
+  });
+
+  it('Returns a different value if a different input is passed in', () => {
+    const trivialKey = deterministicPartitionKey('abc123');
+    const trivialKey2 = deterministicPartitionKey('123abc');
+    expect(trivialKey).not.toBe(trivialKey2);
+  });
+
+  it('Returns a deterministic key of less than the maximum length', () => {
+    const trivialKey = deterministicPartitionKey('abc123adsfdasfdasfadsfdsafsadfaaaaaaaaaaaaaaaaaaafddsafdsfadf');
+    expect(trivialKey.length).toBeLessThanOrEqual(256);
+  });
+
+  it('Returns a key of 128 characters if the input is is not "0"', () => {
+    const trivialKey = deterministicPartitionKey('abc123adsfdasfdasfadsfdsafsadfaaaaaaaaaaaaaaaaaaafddsafdsfadf');
+    expect(trivialKey.length).toBe(128);
+  });
+
+  it('Returns a string that only has valid hex characters in it', () => {
+    const trivialKey = deterministicPartitionKey('ABC12a3');
+    expect(trivialKey).toMatch(/^[0-9a-fA-F]+$/);
+  });
+
+  it('Returns a hash for an event object passed in with a property called "partitionKey"', () => {
+    const event = {
+      partitionKey: '123',
+    };
+    const trivialKey = deterministicPartitionKey(event);
+    console.log(trivialKey);
+    expect(trivialKey).toBe('123');
+  });
+
+  it('Returns a hash for an object passed in without a property called "partitionKey"', () => {
+    const event = {};
+    const trivialKey = deterministicPartitionKey(event);
+    console.log(trivialKey);
+    expect(trivialKey).toMatch(/^[0-9a-fA-F]+$/);
   });
 });
